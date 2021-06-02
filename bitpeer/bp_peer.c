5fc308f608b2e7f3f562f149384efa8acafd7f58
ruyi.chang <ruyi.chang@blockin.com>
2021-06-02 16:37:23 +0800

    [bitpeer] Make jobmaster list fixed.

--- bitpeer/bp_peer.c
+++ bitpeer/bp_peer.c
@@ -852,11 +852,6 @@ static int on_jobmaster_callback(json_t *reply) {
 //    log_info("update jobmaster config success");
 
 
-    char* jb_str ="http://172.17.0.4:5555/newblockmonitor";
-    inetv4_list *jobmaster = malloc(sizeof(inetv4_list));
-    parse_inetv4_addr( jb_str, &jobmaster->arr[1]);
-    settings.jobmaster = jobmaster;
-
     return 0;
 }
 
