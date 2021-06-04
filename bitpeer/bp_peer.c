99e662d9a77e0cfff2290a7bca73c3fe76af5485
ruyi.chang <ruyi.chang@blockin.com>
2021-06-04 18:33:13 +0800

    [bitpeer] Get jobserver list from url .

--- bitpeer/bp_peer.c
+++ bitpeer/bp_peer.c
@@ -271,7 +271,7 @@ static int send_block_nitify(sds hash, int height, uint32_t curtime) {
     json_object_set_new(message, "curtime", json_integer(curtime));
     json_object_set_new(message, "hash", json_string(hash));
     json_object_set_new(message, "prevhash", json_string(previous_has));
-    json_object_set_new(message, "magic", json_integer(4151801719));
+    json_object_set_new(message, "magic", json_string("0xF7777777u"));
 
     sdsfree(previous_has);
 
