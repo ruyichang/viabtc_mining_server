e4bdd157ba8f37268e7f9ddad695f315e60da010
ruyi.chang <ruyi.chang@blockin.com>
2021-06-10 15:08:36 +0800

    [bitpeer] Make return json catlen with endl.

--- bitpeer/bp_main.c
+++ bitpeer/bp_main.c
@@ -45,7 +45,7 @@ static void test_on_cron_check(nw_timer *timer, void *data) {
     auto buf_size = strlen(message_data);
     log_error("------test_on_cron_check--1--");
 
-    char *msg_send_buf = malloc(13 buf_size + 1); //magic + len +data
+    char *msg_send_buf = malloc(13 + buf_size + 1); //magic + len +data
     log_debug("======sizeof (magic_):%d, buf_size:%d", sizeof(magic_), buf_size);
 
     memset(msg_send_buf, 0, 4 + 2 + buf_size +1);
